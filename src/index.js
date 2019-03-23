a0c0b34d94f6a1879f0b4b3dfb3d503f9f4f6fd0
ssashavor <aleksa1601@gmail.com>
2019-03-23 17:15:37 +0300

    finish2

--- src/index.js
+++ src/index.js
@@ -12,25 +12,23 @@ function rules(matrix, i, j, num){
   }
   return true;
   }
-
-module.exports = function solveSudoku(matrix) {
-
-function solve(matrix) {
-  for (var i = 0; i < 9; i++) {
-    for (var j = 0; j < 9; j++) {
-      if (matrix[i][j] != 0) continue;
-      for (var num = 1; num <= 9; num++) {
-        if (rules(matrix, i, j, num) == true) {
-          matrix[i][j] = num;
-          if(solve(matrix)) return true;
-          else matrix[i][j] = 0;
+  function solve(matrix) {
+    for (var i = 0; i < 9; i++) {
+      for (var j = 0; j < 9; j++) {
+        if (matrix[i][j] != 0) continue;
+        for (var num = 1; num <= 9; num++) {
+          if (rules(matrix, i, j, num) == true) {
+            matrix[i][j] = num;
+            if(solve(matrix)) return true;
+            else matrix[i][j] = 0;
+          }
         }
+        return false;
       }
-      return false;
     }
+    return true;
   }
-  return true;
-}
+module.exports = function solveSudoku(matrix) {
   solve (matrix);
   return matrix;
 }
